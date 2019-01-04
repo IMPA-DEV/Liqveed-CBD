6f040a1be37720d434fcc9a25e917d22a888dd99
MAXON-BY <maxon.pro2015@gmail.com>
2019-01-04 17:56:44 +0300

    Работа над главной страницей

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,28 @@
 $(function() {
 
-	// Custom JS
+    // Page scroll to id
+    $(".scroll-to a").mPageScroll2id({
+        offset: 90,
+        duration: 900
+    });
+    // $.mPageScroll2id("scrollTo","#top",{
+    //     offset:0
+    // });
+
+    //scroll top-link transparent
+    $(function() {
+        var header = $("#top-line");
+        $(window).scroll(function(scrlevt) {
+            scrlevt.preventDefault();
+            var scroll = $(window).scrollTop();
+
+            if (scroll > 1) {
+                header.addClass("scroll");
+            } else {
+                header.removeClass("scroll");
+            }
+            return false;
+        });
+    });
 
 });
