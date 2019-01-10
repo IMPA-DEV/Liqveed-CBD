8d1f87aa29fe430c79ff0674ac2fbfc433c487aa
MAXON-BY <maxon.pro2015@gmail.com>
2019-01-10 18:48:18 +0300

    Сделана страница order.html

--- app/js/common.js
+++ app/js/common.js
@@ -25,4 +25,27 @@ $(function() {
         });
     });
 
+    /* slick slider */
+    $('.testimonials-slider').slick({
+        centerMode: true,
+        infinite: true,
+        centerPadding: '0px',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: $('.testimonials-prev'),
+        nextArrow: $('.testimonials-next')
+    });
+
+    /* check form final step payment */
+    $('#form-check__address').on('click', function () {
+        var payAdr = $('.payment-form__address');
+        if ($(this).is(':checked')) {
+            payAdr.slideUp(300);
+        } else {
+            payAdr.slideDown(300);
+            payAdr.children('input').each(function () {
+                $(this).prop('required', true);
+            });
+        }
+    });
 });
